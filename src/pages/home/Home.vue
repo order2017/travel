a08451cfb9257c22c01af7e5e42975f948c4741a
403228075@qq.com <403228075@qq.com>
2019-06-29 19:02:08 +0800

    未完成ICONS

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div id="home">
   <home-header></home-header>
   <home-swiper></home-swiper>
+  <home-icon></home-icon>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
+import HomeIcon from './components/Icons.vue'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcon
   }
 }
 </script>
